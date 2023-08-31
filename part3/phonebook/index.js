1f8434fc4a0899191c179d0c4c7139f9480ae785
MarioPerac <mario.perac@student.etf.unibl.org>
2023-08-31 22:43:20 +0200

    Add the app phonebook, completed exercises [7-8] - part 3

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,21 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
+morgan.token('body', (req, res) => {
+    if (req.method === 'POST')
+        return JSON.stringify(req.body)
+})
 app.use(express.json())
+app.use(morgan(function (tokens, req, res) {
+    return [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, 'content-length'), '-',
+        tokens['response-time'](req, res), 'ms',
+        tokens.body(req, res)
+    ].join(' ')
+}))
 
 let persons = [
     {
@@ -47,7 +62,7 @@ app.get('/api/persons/:id', (req, res) => {
         res.json(person)
     }
     else {
-        response.status(404).end()
+        res.status(404).end()
     }
 
 })
@@ -75,7 +90,7 @@ app.post('/api/persons', (req, res) => {
     }
 
     const id = Math.floor(Math.random() * 1000)
-    let person = req.body
+    let person = { ...req.body }
     person.id = id
     persons = persons.concat(person)
     res.json(person)
