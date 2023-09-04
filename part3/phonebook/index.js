b5cb6460231319ac0d99fd797756baa4d3111ee4
MarioPerac <mario.perac@student.etf.unibl.org>
2023-09-04 19:29:02 +0200

    Add the app phonebook, completed exercises [9] - part 3

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const app = express()
+
+const cors = require('cors')
+app.use(cors())
+
 const morgan = require('morgan')
 morgan.token('body', (req, res) => {
     if (req.method === 'POST')
